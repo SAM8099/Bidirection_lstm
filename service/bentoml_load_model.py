567cb8ed289948b948c8169dfa2d735bc844bac9
SAM8099 <samarthgarg8099@gmail.com>
2024-07-24 20:22:46 +0530

    savedmodel format comp

--- service/bentoml_load_model.py
+++ service/bentoml_load_model.py
@@ -4,9 +4,6 @@ import bentoml
 
 def load_model_and_save_it_in_bentoml(model_file:Path) -> None:
     model = tf.keras.models.load_model(model_file)
-    bento_model = bentoml.keras.save_model("bento_keras_model", model)
+    bento_model = bentoml.tensorflow.save_model("bento_keras_model", model)
     print(f"Bento_model : {bento_model.tag}")
     
-if __name__=="__main__":
-    model_file = Path("BI_LSTM_model.h5")
-    load_model_and_save_it_in_bentoml(model_file)
